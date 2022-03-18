11efca5f6e4be21bd96306e2293a969c45b2a425
Trevor Settles <roboteng@gmail.com>
2022-03-17 20:35:59 -0600

    started new note form

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,11 @@ module Main exposing (..)
 import Browser
 import Css exposing (..)
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (css)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (..)
 
 
-main : Program () {} a
+main : Program () Model Msg
 main =
     Browser.sandbox
         { init = init
@@ -16,28 +17,62 @@ main =
 
 
 type alias Model =
-    {}
+    { newNote : Maybe NewNoteForm }
 
 
-init : {}
+type alias NewNoteForm =
+    { title : String
+    , description : String
+    }
+
+
+type Msg
+    = EditNote
+    | SaveNote
+
+
+init : Model
 init =
-    {}
+    { newNote = Nothing
+    }
 
 
-update : a -> Model -> Model
-update _ x =
-    x
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        EditNote ->
+            { model
+                | newNote = Just { title = "", description = "" }
+            }
+
+        SaveNote ->
+            { model | newNote = Nothing }
 
 
 
 -- VIEW
 
 
-view : Model -> Html a
-view _ =
+view : Model -> Html Msg
+view model =
     div
         [ css
             [ backgroundColor (rgb 100 100 100), color (rgb 200 200 200) ]
         ]
-        [ h1 [] [ text "Notes" ]
+        [ nav []
+            [ h1 [] [ text "Notes" ]
+            ]
+        , main_ []
+            [ button [ onClick EditNote ] [ text "Add New Note" ]
+            , case model.newNote of
+                Just note ->
+                    Html.Styled.form [ onSubmit SaveNote ]
+                        [ input [ value note.title ] []
+                        , input [ value note.description ] []
+                        , button [ onClick SaveNote ] [ text "Save" ]
+                        ]
+
+                Nothing ->
+                    div [] []
+            ]
         ]
