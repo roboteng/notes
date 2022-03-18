2dadf7813612c2409cd5bcff799237c5914faadd
Trevor Settles <roboteng@gmail.com>
2022-03-17 21:59:45 -0600

    added saveNote test

--- tests/Example.elm
+++ tests/Example.elm
@@ -8,13 +8,43 @@ import Test exposing (Test, describe, test)
 suite : Test
 suite =
     describe "Given an empty model"
-        [ describe
+        (let
+            model =
+                init
+         in
+         [ describe
             "WHen the user clicks on the Add Note Button"
-            [ let
+            (let
                 action =
                     EditNote
-              in
-              test "Then we have state for entry"
-                (\_ -> Expect.equal (update action init) { init | newNote = Just initNote })
-            ]
-        ]
+             in
+             [ test "Then we have state for entry"
+                (\_ -> Expect.equal (update action model) { model | newNote = Just initNote })
+             ]
+            )
+         ]
+        )
+
+
+other : Test
+other =
+    describe "Given the Edit Note page is open"
+        (let
+            model =
+                { init | newNote = Just { title = "Title", description = "Description" } }
+         in
+         [ describe "When the user save a note with data"
+            (let
+                action =
+                    SaveNote
+             in
+             [ test "Then the newNote is Nothing"
+                (\_ ->
+                    Expect.equal
+                        { notes = [ { title = "Title", description = "Description" } ], newNote = Nothing }
+                        (update action model)
+                )
+             ]
+            )
+         ]
+        )
